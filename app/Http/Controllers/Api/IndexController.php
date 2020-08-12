2a760a2181b5f52f59ca91098c735c92419318b8
1078979110 <1078979110@qq.com>
2020-08-12 19:08:03 +0800

    个人中心手机号，如果没有授权，则显示为空

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -302,9 +302,9 @@ class IndexController extends Controller
         $customer = Customer::with('address')->where('openid', $openid)->first();
 
         $cart_num = Cart::wherehas('product')->where('customer_id', $customer->id)->count();
-        if (empty($customer->tel)) {
+        /* if (empty($customer->tel)) {
             $customer['tel'] = $customer->address['tel'];
-        }
+        } */
 
         $customer['cart_num'] = $cart_num;
 
