2dd2cc49073677131aff6a5405f02cb08b31abfa
1078979110 <1078979110@qq.com>
2020-08-19 20:15:19 +0800

    商品审核

---
+++ app/Admin/Controllers/Live/LivegoodsController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MNRC
+ * Date: 2020/8/19
+ * Time: 19:57
+ */
+namespace App\Admin\Controllers\Live;
+
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+use App\Models\Live\Live;
+class LivegoodsController extends AdminController{
+
+}
