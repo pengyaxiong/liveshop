93fbe4ff77fbb7db955dd44c9ba252bb6e6a8707
1078979110 <1078979110@qq.com>
2020-08-25 15:13:38 +0800

    获取首屏第一个直播或者录播

--- app/Admin/Controllers/Live/LiveController.php
+++ app/Admin/Controllers/Live/LiveController.php
@@ -13,54 +13,20 @@ class LiveController extends AdminController{
     
     protected function grid(){
         $grid = new Grid(new Live());
-        $Wechat = new WeChat();
-        $Wechat->updateLives();
         $grid->filter(function($filter){
             $filter->disableIdFilter();
             $filter->equal('name','直播间名');
-            $filter->equal('room_id','直播间ID');
         });
-        /*$grid->column('id','ID')->sortable();*/
-        $grid->column('room_id','主播间ID')->sortable();
-        $grid->column('name', '直播间名');
-
-        $grid->column('start_time','开始时间')->display(function($start_time){
-            return date('Y-m-d H:i:s', $start_time);
-        });
-        $grid->column('end_time','结束时间')->display(function($end_time){
-            return date('Y-m-d H:i:s', $end_time);
-        });
-        $grid->column('anchor_name','主播昵称');
-        $grid->column('rtmp','推流地址')->editable()->width(250);
-        $grid->column('group_id','聊天室ID')->editable();
-        $grid->column('live_status', '直播状态')->display(function(){
-            $str = '';
-            switch ($this->live_status){
-                case 101:
-                    $str = '直播中';
-                    break;
-                case 102:
-                    $str = '未开播';
-                    break;
-                case 103:
-                    $str = '已结束';
-                    break;
-                case 104:
-                    $str = '禁播';
-                    break;
-                case 105:
-                    $str = '暂停';
-                    break;
-                case 106:
-                    $str = '异常';
-                    break;
-                default:
-                case 107:
-                    $str = '已过期';
-                    break;
-            }
-            return $str;
-        });
+        $grid->column('openid','openid');
+        $grid->column('streamname','直播间标识');
+        $grid->column('nickname','主播昵称');
+        $grid->column('avator','主播头像')->image('',50,50);
+        $grid->column('title','直播间标题');
+        $grid->column('pushurl','推流地址');
+        $grid->column('playurl','播放地址');
+        $grid->column('StreamState','状态');
+        $grid->column('group_id','聊天室id')->editable()->help('请前往腾讯IM控制台获取');
+        $grid->column('goods','货架商品');
         $grid->disableActions();
         $grid->disableExport();
         $grid->disableColumnSelector();
@@ -74,7 +40,6 @@ class LiveController extends AdminController{
 
     protected function Form(){
         $form = new Form(new Live());
-        $form->text('rtmp','推流地址');
         $form->text('group_id','聊天室ID');
         return $form;
     }
