3cf8fdee640ad618b289bbb60f6336e055a77a64
1078979110 <1078979110@qq.com>
2020-08-25 09:55:30 +0800

    获取首屏第一个直播或者录播

--- app/Http/Controllers/Api/LiveController.php
+++ app/Http/Controllers/Api/LiveController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Handlers\TLSSigAPIv2;
 use App\Http\Controllers\Controller;
 use TencentCloud\Common\Credential;
 use TencentCloud\Common\Profile\ClientProfile;
 use TencentCloud\Common\Profile\HttpProfile;
 use TencentCloud\Common\Exception\TencentCloudSDKException;
 use TencentCloud\Live\V20180801\LiveClient;
+use TencentCloud\Live\V20180801\Models\CreateLiveSnapshotTemplateRequest;
 use TencentCloud\Live\V20180801\Models\DescribeLiveRecordTemplatesRequest;
 use TencentCloud\Live\V20180801\Models\CreateLiveRecordTemplateRequest;
 use TencentCloud\Live\V20180801\Models\DeleteLiveRecordTemplateRequest;
@@ -29,6 +31,9 @@ class LiveController extends Controller
     protected $PushDomain ;
     protected $PlayDomain ;
     protected $Key ;
+    protected $Imappid;
+    protected $Imkey;
+    protected $Imadmin;
     public function __construct()
     {
         $this->SecretId = env('SecretId');
@@ -36,6 +41,11 @@ class LiveController extends Controller
         $this->PushDomain = env('LIVE_PUSH_DOMAIN');
         $this->PlayDomain = env('LIVE_PLAY_DOMAIN');
         $this->Key = env('LIVE_KEY');
+
+        //im 聊天appid && key
+        $this->Imappid = env('IM_SDK_APPID','');
+        $this->Imkey = env('IM_SDK_KEY','');
+        $this->Imadmin = env('IM_ADMIN','');
     }
 
     public function DescribeLiveRecordTemplates(Request $request)
@@ -243,6 +253,31 @@ class LiveController extends Controller
         }
     }
 
+    /*
+     * 鉴黄模板创建
+     */
+    public function CreateLiveSnapshotTemplate(Request $request){
+        try{
+            $cred = new Credential($this->SecretId, $this->SecretKey);
+            $httpProfile = new HttpProfile();
+            $clientProfile = new ClientProfile();
+            $clientProfile->setHttpProfile($httpProfile);
+            $client = new LiveClient($cred, "ap-guangzhou", $clientProfile);
+            $req = new CreateLiveSnapshotTemplateRequest();
+            $params = array(
+                'TemplateName' => $request->TemplateName,
+                'CosAppId' => $request->CosAppId,
+                'CosBucket' => $request->CosBucket,
+                'CosRegion' => $request->CosRegion
+            );
+            $req->formJsonString(json_encode($params));
+            $resp = $client->CreateLiveSnapshotTemplate($req);
+            print_r($resp->toJsonString());
+        } catch (TencentCloudSDKException $e){
+            echo $e;
+        }
+    }
+
     public function DescribeLiveStreamOnlineListFirst(Request $request){
         try {
 
@@ -349,17 +384,24 @@ class LiveController extends Controller
         }
     }
 
+    public function DescribeLiveStreamState(Request $request){
+
+    }
+
     public function CreatePush(Request $request)
     {
         $roomTitle = $request->roomTitle;
         $openId = $request->openId;
         $nickName = $request->nickName;
         $vataor = $request->vataor;
+        $userId = $request->userId;
         $time = $request->endTime ? $request->endTime : date('Y-m-d H:i:s', strtotime('+3 hours'));
         $pushUrl = $this->getPushUrl($this->PushDomain, $openId, $this->Key, $time);
         $playUrl = $this->getPlayUrl($this->PlayDomain, $openId);
+
         $info = DB::table('live_rooms')->where('openid',$openId)->find();
         if(empty($info)){
+            $group_id = $this->createChatRoom(['userid'=>$userId, 'name'=> $nickName]);
             $data = ['openid'=>$openId, 'streamname'=>$openId,'nickname'=>$nickName, 'title'=>$roomTitle, 'avator'=>$vataor, 'pushurl'=>$pushUrl, 'playurl'=>$playUrl,'created_at'=>time()];
             $result = DB::table('live_rooms')->insert($data);
         }else{
@@ -443,4 +485,126 @@ class LiveController extends Controller
         }
     }
 
+    /**生成genSig
+     * @param Request $request
+     * @return array
+     */
+    public function makeGenSig($userId){
+        $TLSSig = new TLSSigAPIv2($this->Imappid, $this->Imkey);
+        $genSign = $TLSSig->genSig($userId);
+        return $genSign;
+    }
+
+    /**生成genSig
+     * @param Request $request
+     * @return array
+     */
+    public function makeGenSigForRequest(Request $request){
+        $userId = $request->userid;
+        $TLSSig = new TLSSigAPIv2($this->Imappid, $this->Imkey);
+        $genSign = $TLSSig->genSig($userId);
+        return $this->success_data('UserSig',['userID'=>$userId, 'UserSig'=>$genSign]);
+    }
+
+
+    /**创建直播聊天室
+     * @param $data 创建聊天室必须字段
+     *          anchor_wechat 主播微信号
+     *          anchor_name 主播昵称
+     *          name 聊天室名称
+     *          group_id 聊天室id 选填
+     * @return array
+     *          ActionStatus string 请求处理的结果，OK 表示处理成功，FAIL 表示失败
+     *          ErrorCode int 错误码，0表示成功，非0表示失败
+     *          ErrorInfo string 错误信息
+     *          GroupId String 创建成功之后的群 ID，由即时通信 IM 后台分配
+     */
+    public function createChatRoom($data){
+        $usersig = $this->makeGenSig($this->Imadmin);
+        $random = rand(100000000,99999999).rand(100000000,99999999).rand(100000000,99999999);
+        $url = "https://console.tim.qq.com/v4/group_open_http_svc/create_group?sdkappid={$this->Imappid}&identifier={$this->Imadmin}&usersig={$usersig}&random={$random}&contenttype=json";
+        $groupData = [
+            'Owner_Account' => $data['userId'],
+            'Type' => 'Public',
+            'Name' => $data['name'],
+            'ApplyJoinOption' => 'FreeAccess'
+        ];
+        if($data['group_id']){
+            $groupData['GroupId'] = $data['group_id'];
+        }
+        $result = $this->exportChater(['userId'=>$data['userId'],'name'=>$data['name']]);
+        if($result['ErrorCode'] == 0){
+            $res = $this->postHttp($url, json_encode($groupData));
+            $re = json_decode($res,true);
+            return $re;
+        }else{
+            return $result;
+        }
+
+    }
+
+    /**导入单个账号用作创建直播间的群组，作为聊天室群主
+     * @param $data
+     *          anchor_wechat 微信号
+     *          anchor_name 昵称
+     * @return  array
+     *          ActionStatus String 请求处理的结果，OK 表示处理成功，FAIL 表示失败
+     *          ErrorCode int 错误码，0表示成功，非0表示失败
+     *          ErrorInfo  String 错误信息
+     */
+    public function exportChater($data){
+        $usersig = $this->makeGenSig($this->Imadmin);
+        $random = rand(100000000,99999999).rand(100000000,99999999).rand(100000000,99999999);
+        $url = "https://console.tim.qq.com/v4/im_open_login_svc/account_import?sdkappid={$this->Imappid}&identifier={$this->Imadmin}&usersig={$usersig}&random={$random}&contenttype=json";
+        $data = [
+            'Identifier' => $data['userId'],
+            'Nick' => $data['name']
+        ];
+        $result = $this->postHttp($url, json_encode($data));
+        $res = json_decode($result, true);
+        return $res;
+    }
+
+
+
+
+    //获取GET请求
+    function getHttp($url)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 500);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        $res = curl_exec($curl);
+        curl_close($curl);
+        return $res;
+    }
+
+    /**
+     *
+     * @param string url 目的地址
+     * @param array data 要传递的数据
+     * @param array|null header 头部信息
+     * @return mixed
+     */
+    function postHttp($url, $data, $header=[]){
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        if($header){
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+        }
+        curl_setopt( $curl, CURLOPT_SAFE_UPLOAD, true);
+        if (! empty($data)) {
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($curl);
+        curl_close($curl);
+        return $output;
+    }
 }
