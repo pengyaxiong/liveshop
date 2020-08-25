882c6793294adfe332b7e5b89e580c0ea49d7b93
1078979110 <1078979110@qq.com>
2020-08-25 17:18:42 +0800

    添加商品

--- app/Admin/Controllers/Live/LiveController.php
+++ app/Admin/Controllers/Live/LiveController.php
@@ -44,9 +44,7 @@ class LiveController extends AdminController{
         });
         $grid->column('操作')->display(function(){
             return '<button class="btn btn-primary btn-xs">添加商品</button>';
-        })->modal('添加商品',function(){
-
-        })->ajax('/admin/live/api/getproducts');
+        })->modal('添加商品','/admin/live/api/getproducts');
         $grid->disableActions();
         $grid->disableExport();
         $grid->disableColumnSelector();
