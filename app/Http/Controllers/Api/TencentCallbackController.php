1d6ab64d37155e2d367221109f7e550678185bd6
1078979110 <1078979110@qq.com>
2020-08-25 11:03:26 +0800

    添加接收消息通知

--- app/Http/Controllers/Api/TencentCallbackController.php
+++ app/Http/Controllers/Api/TencentCallbackController.php
@@ -12,15 +12,36 @@ use App\Http\Controllers\Controller;
 
 class TencentCallbackController extends Controller
 {
+    /*
+     * 接收腾讯回调
+     */
     public function LiveCallback(Request $request){
         $callbackData = json_decode($request->data);
-        if($callbackData['event_type'] == 0){//断流通知
+        switch ($callbackData['event_type']){
+            case 0://断流通知
+                $data['StreamState'] = 'inactive';
+                $result = DB::table('live_rooms')->where('streamname', $callbackData['stream_id'])->update($data);
+                break;
+            case 1://推流通知
+                $data['StreamState'] = 'active';
+                $result = DB::table('live_rooms')->where('streamname', $callbackData['stream_id'])->update($data);
+                break;
+            case 100://录制通知
+                break;
+            case 200://直播截图
+                break;
+            case 317://鉴黄通知
+                break;
+            default:
+                break;
+        }
+        /*if($callbackData['event_type'] == 0){//断流通知
             $data['StreamState'] = 'inactive';
         }else if($callbackData['event_type'] == 1){//推流通知
             $data['StreamState'] = 'active';
         }else{
             $data['StreamState'] = 'forbid';
         }
-        $result = DB::table('live_rooms')->where('streamname', $callbackData['stream_id'])->update($data);
+        $result = DB::table('live_rooms')->where('streamname', $callbackData['stream_id'])->update($data);*/
     }
 }
