e3647b1e635058c46bbabb1e59d160f2af6111e9
1078979110 <1078979110@qq.com>
2020-08-25 15:18:23 +0800

    获取首屏第一个直播或者录播

--- app/Admin/Controllers/Live/LiveController.php
+++ app/Admin/Controllers/Live/LiveController.php
@@ -15,7 +15,8 @@ class LiveController extends AdminController{
         $grid = new Grid(new Live());
         $grid->filter(function($filter){
             $filter->disableIdFilter();
-            $filter->equal('name','直播间名');
+            $filter->equal('title','直播间标题');
+            $filter->like('nickname','主播昵称');
         });
         $grid->column('openid','openid');
         $grid->column('streamname','直播间标识');
