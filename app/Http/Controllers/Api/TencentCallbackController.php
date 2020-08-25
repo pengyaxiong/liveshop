6164e85b1694a2f62cdfdea6169203c6daa84052
1078979110 <1078979110@qq.com>
2020-08-25 11:57:09 +0800

    获取首屏第一个直播或者录播

--- app/Http/Controllers/Api/TencentCallbackController.php
+++ app/Http/Controllers/Api/TencentCallbackController.php
@@ -19,16 +19,16 @@ class TencentCallbackController extends Controller
         $data = file_get_contents("php://input");
         file_put_contents(storage_path('logs/callback.log'),$data);
         $callbackData = json_decode($data, true);
-        switch ($callbackData['event_type']){
+        switch ($callbackData->event_type){
             case 0://断流通知
             case '0':
                 $data_['StreamState'] = 'inactive';
-                $result = DB::table('live_rooms')->where('streamname', $callbackData['stream_id'])->update($data_);
+                $result = DB::table('live_rooms')->where('streamname', $callbackData->stream_id)->update($data_);
                 break;
             case 1://推流通知
             case '1':
                 $data_['StreamState'] = 'active';
-                $result = DB::table('live_rooms')->where('streamname', $callbackData['stream_id'])->update($data_);
+                $result = DB::table('live_rooms')->where('streamname', $callbackData->stream_id)->update($data_);
                 break;
             case 100://录制通知
             case '100':
