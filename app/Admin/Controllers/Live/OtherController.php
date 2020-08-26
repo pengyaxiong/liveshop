97ec5aafef03a5252979b057a7b24b8a4d2c0646
1078979110 <1078979110@qq.com>
2020-08-26 15:11:14 +0800

    直播间上架商品

---
+++ app/Admin/Controllers/Live/OtherController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MNRC
+ * Date: 2020/8/26
+ * Time: 11:22
+ */
+
+namespace App\Admin\Controllers\Live;
+
+use App\Models\Shop\Product;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Widgets\Form;
+use Illuminate\Http\Request;
+use Encore\Admin\Layout\Content;
+use App\Models\Live\Live;
+use Illuminate\Support\Facades\DB;
+class OtherController extends AdminController
+{
+    public function editGoods(Content $content){
+        $content->title('直播间货架');
+        $request = request();
+        $id = $request->id;
+        $roomInfo = Live::find($id)->toArray(true);
+        $form = new Form($roomInfo);
+        $form->method('post');
+        $form->action('/admin/live/setgoods');
+        $form->hidden('id');
+        $form->text('title','直播间主题')->disable();
+        $form->text('nickname','主播昵称')->disable();
+        $form->multipleSelect('goods','商品')->options(function($goods){
+            if(empty($goods)){
+                return [];
+            }else{
+                $lists = Product::whereIn('id',$goods)->pluck('name','id');
+                return $lists;
+            }
+        })->ajax('/admin/live/getproducts');
+        $content->body($form);
+        return $content;
+    }
+}
