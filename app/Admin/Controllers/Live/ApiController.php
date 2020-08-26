8507e532b8fb71c3d3732f8a386fb1a46930ea59
1078979110 <1078979110@qq.com>
2020-08-26 16:46:37 +0800

    添加商品

--- app/Admin/Controllers/Live/ApiController.php
+++ app/Admin/Controllers/Live/ApiController.php
@@ -8,6 +8,7 @@
 
 namespace App\Admin\Controllers\Live;
 
+use App\Models\Shop\Coupon;
 use Encore\Admin\Controllers\AdminController;
 use App\Models\Shop\Product;
 use Illuminate\Http\Request;
@@ -18,18 +19,9 @@ class ApiController extends AdminController{
         $lists = Product::where('name','like','%'.$q.'%')->where('is_show',1)->paginate(null,['id','name as text']);
         return $lists;
     }
-    public function rollBackGoods(Request $request){
-        $id = $request->id;
-    }
-
-    public function setGoods(Request $request){
-        if($request->isMethod('post')){
-            $id = $request->id;
-            $goods = array_filter($request->goods);
-            $data['goods'] = join(',',$goods);
-            $result = Live::where('id', $id)->update($data);
-            admin_toastr('添加成功','success');
-            return redirect('/admin/live/rooms');
-        }
+    public function getCoupons(Request $request){
+        $q = $request->q;
+        $lists = Coupon::where('name','like','%'.$q.'%')->paginate(null,['id','name as text']);
+        return $lists;
     }
 }
