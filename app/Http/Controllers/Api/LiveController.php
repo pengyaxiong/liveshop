75076294a74a000aa33573c61a4306e1c8bcc282
1078979110 <1078979110@qq.com>
2020-08-26 15:33:45 +0800

    添加商品

--- app/Http/Controllers/Api/LiveController.php
+++ app/Http/Controllers/Api/LiveController.php
@@ -688,6 +688,7 @@ class LiveController extends Controller
         foreach ($goodsArr as $key=>$id){
             $info = Product::find($id);
             $info['image'] = env('APP_URL').'/storage/'.$info['image'];
+            var_dump($info['images']);
             foreach (json_decode($info['images'], true) as $k => $item){
                 $info['images'][$k] = env('APP_URL').'/storage/'.$item;
             }
