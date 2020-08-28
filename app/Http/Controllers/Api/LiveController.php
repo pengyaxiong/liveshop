d73f7d1694a4f139f8bd6094247cfa5a1807bc10
1078979110 <1078979110@qq.com>
2020-08-28 16:23:17 +0800

    添加商品

--- app/Http/Controllers/Api/LiveController.php
+++ app/Http/Controllers/Api/LiveController.php
@@ -692,7 +692,7 @@ class LiveController extends Controller
         $date = date('Ymd', time());
         $has_stream = DB::table('live_rooms_view')->where([['room_id',$room_id],['view_date',$date]])->exists();
         if($has_stream){
-            $result = DB::table('live_rooms_view')->where([['room_id',$room_id],['view_date',$date]])->increment('view_num',1,['updated_at'=>date('Y-m-d H:i:s', time()]);
+            $result = DB::table('live_rooms_view')->where([['room_id',$room_id],['view_date',$date]])->increment('view_num',1,['updated_at'=>date('Y-m-d H:i:s', time())]);
         }else{
             $data = ['room_id'=>$room_id,'view_num'=>1,'view_date'=>$date,'created_at'=>date('Y-m-d H:i:s', time())];
             $result = DB::table('live_rooms_view')->insert($data);
