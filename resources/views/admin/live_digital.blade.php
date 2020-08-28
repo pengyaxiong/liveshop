b71debed0cb710b953d84a5c1984c97b07f0b373
1078979110 <1078979110@qq.com>
2020-08-28 17:48:30 +0800

    添加商品

---
+++ resources/views/admin/live_digital.blade.php
@@ -0,0 +1,10 @@
+<div id="order_count" style="height: 400px;width: 100%"></div>
+<script src="https://cdn.jsdelivr.net/npm/echarts@4.8.0/dist/echarts.min.js"></script>
+<script src="/vendor/echarts/macarons.js"></script>
+<script src="/vendor/echarts/china.js"></script>
+<script>
+    var stream = "{{$stream}}";
+    $(function () {
+
+    });
+</script>
