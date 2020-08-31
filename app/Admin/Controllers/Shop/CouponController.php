089f54e9d1493fbeafc6c9d7b3c4dc39f6d25d20
1078979110 <1078979110@qq.com>
2020-08-31 14:38:14 +0800

    添加商品

--- app/Admin/Controllers/Shop/CouponController.php
+++ app/Admin/Controllers/Shop/CouponController.php
@@ -77,8 +77,8 @@ class CouponController extends AdminController
         $form->decimal('cut', __('减'))->rules('required');
         $form->number('totalnum','总数量');
         $form->number('limitnum', '限领数量');
-        $form->date('invalidate','有效期')->options(function($invalidate){
-            return date('Y-m-d', $invalidate);
+        $form->date('invalidate','有效期')->options(function(){
+            return date('Y-m-d', $this->invalidate);
         });
         $form->saving(function($model){
             $model->residue = $model->totalnum;
