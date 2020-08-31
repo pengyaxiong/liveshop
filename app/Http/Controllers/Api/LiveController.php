30c7d6fd242aee037ebe957cff76cc6bbe258fcf
1078979110 <1078979110@qq.com>
2020-08-31 16:25:31 +0800

    添加商品

--- app/Http/Controllers/Api/LiveController.php
+++ app/Http/Controllers/Api/LiveController.php
@@ -829,6 +829,7 @@ class LiveController extends Controller
             $couponinfo = Coupon::find($val->coupon_id);
             $coupons[$key]->price = $couponinfo->price;
             $coupons[$key]->cut = $couponinfo->cut;
+            $coupons[$key]->status = 5;
         }
         return $this->success_data('可用优惠券列表',['list'=>$coupons]);
     }
