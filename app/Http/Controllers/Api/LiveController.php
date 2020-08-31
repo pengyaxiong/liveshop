3ee517f1e216d9ae7e1335900021a58681fbc170
1078979110 <1078979110@qq.com>
2020-08-31 09:20:16 +0800

    添加商品

--- app/Http/Controllers/Api/LiveController.php
+++ app/Http/Controllers/Api/LiveController.php
@@ -721,9 +721,9 @@ class LiveController extends Controller
         foreach ($couponArr as $key=>$id){
             $info = Coupon::find($id)->toArray(true);
             $info['status'] = 0;
-            $is_get = DB::table('shop_customer_coupon')->where([['coupon_id'=>$id],['customer_id'=>$openid]])->exists();
+            $is_get = DB::table('shop_customer_coupon')->where([['coupon_id',$id],['customer_id',$openid]])->exists();
             if($is_get){
-                $info['status'] = DB::table('shop_customer_coupon')->where([['coupon_id'=>$id],['customer_id'=>$openid]])->value('status');
+                $info['status'] = DB::table('shop_customer_coupon')->where([['coupon_id',$id],['customer_id',$openid]])->value('status');
             }
             $couponList[] = $info;
         }
