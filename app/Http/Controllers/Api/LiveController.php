2b2731e542dca6db7f56103adace66e165b86997
1078979110 <1078979110@qq.com>
2020-08-31 10:50:23 +0800

    添加商品

--- app/Http/Controllers/Api/LiveController.php
+++ app/Http/Controllers/Api/LiveController.php
@@ -723,6 +723,7 @@ class LiveController extends Controller
             $info = Coupon::find($id)->toArray(true);
             $info['status'] = 0;//未领取
             $is_get = DB::table('shop_customer_coupon')->where([['coupon_id',$id],['customer_id',$customerid]])->exists();
+            var_dump($is_get);
             if($is_get){
                 $info['status'] = 1;//已经领取
             }
